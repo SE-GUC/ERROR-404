6b96c1ca1c62d31503a75001052550dbda5f55cb
Bassant Elnaggar <47534992+bassantelnaggar@users.noreply.github.com>
2019-02-28 17:55:18 +0200

    Update update.js

--- routes/api/update.js
+++ routes/api/update.js
@@ -5,55 +5,57 @@ const update = require('../../models/user');
 //temp data for testing
 const users = [
     {
-        type:'member',
+       ,
         firstname:'Mahy',
         lastname: 'Ali',
         birth_date:'23-4-1994',
-        bio: 'expert debater ',
+        clubs:['TIQ'],
         email: 'mali@gmail.com',
         password:'mahy94',
+        type:'member',
         house:'Pegasus',
         score: 278,
         din:'1-10-2017',
         dor:null,
-        clubs:['TIQ'],
-        id : '1'
+        bio: 'expert debater '
+        id : 1
 
     }];
     router.get('/', (req, res) => res.json({ data: users }))
 
-   
-    router.put('/api/update/:id',(req,res)=>{
-        const userid =req.params.id;
+   // updating the info/profile of a user
+    router.put('/',(req,res)=>{
+        const userid =req.body.id;
+        
         const updatedfname= req.body.firstname
         const updatedlname = req.body.lastname
-        const updatedemail = req.body.email
-        const updatedpass = req.body.password
         const updatedbdate = req.body.birth_date
-        const updatedbio = req.body.bio
+         const updatedclubs = req.body.clubs
+        const updatedemail = req.body.email
+         const updatedpass = req.body.password
+         const updatedtype=req.body.type
         const updatedhouse = req.body.house
         const updatedscore = req.body.score
         const updatedin = req.body.din
         const updatedor = req.body.dor
-        const updatedclubs = req.body.clubs
-        const userf = users.find(user => user.id === userid )
-        userf.firstname = updatedfname
+       const updatedbio = req.body.bio
+       const userf = users.find(user => user.id === userid )
+      
+         userf.firstname = updatedfname
         userf.lastname = updatedlname
+         userf.birth_date = updatedbdate
+        userf.clubs = updatedclubs
         userf.email = updatedemail
         userf.password = updatedpass
-        userf.birth_date = updatedbdate
-        userf.bio = updatedbio
-        userf.house = updatedhouse
-        userf.score = updatedscore
-        userf.din = updatedin
-        userf.dor = updatedor
-        userf.clubs = updatedclubs
+         userf.type=updatedtype
+         userf.house = updatedhouse
+         userf.score = updatedscore
+          userf.din = updatedin
+         userf.dor = updatedor
+         userf.bio = updatedbio
+         
 
     res.send(users)
     })
-    // Questions :
-     //validaion for the new inputs?
-    //schema for who should be able to update?
-
- 
+   
 module.exports = router
