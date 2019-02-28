65f020d6bf5d9139fcba84bc3a0313e738eb5021
Bassant Elnaggar <47534992+bassantelnaggar@users.noreply.github.com>
2019-02-28 19:58:39 +0200

    Update update.js

--- routes/api/update.js
+++ routes/api/update.js
@@ -5,19 +5,18 @@ const update = require('../../models/user');
 //temp data for testing
 const users = [
     {
-       
+        type:'member',
         firstname:'Mahy',
         lastname: 'Ali',
         birth_date:'23-4-1994',
-        clubs:['TIQ'],
+        bio: 'expert debater ',
         email: 'mali@gmail.com',
         password:'mahy94',
-        type:'member',
         house:'Pegasus',
         score: 278,
         din:'1-10-2017',
         dor:null,
-        bio: 'expert debater '
+        clubs:['TIQ'],
         id : 1
 
     }];
@@ -26,36 +25,44 @@ const users = [
    // updating the info/profile of a user
     router.put('/',(req,res)=>{
         const userid =req.body.id;
-        
+        const updatedtype=req.body.type
         const updatedfname= req.body.firstname
         const updatedlname = req.body.lastname
-        const updatedbdate = req.body.birth_date
-         const updatedclubs = req.body.clubs
         const updatedemail = req.body.email
          const updatedpass = req.body.password
-         const updatedtype=req.body.type
+        const updatedbdate = req.body.birth_date
+        const updatedbio = req.body.bio
         const updatedhouse = req.body.house
         const updatedscore = req.body.score
         const updatedin = req.body.din
-        const updatedor = req.body.dor
-       const updatedbio = req.body.bio
+       const updatedor = req.body.dor
+        const updatedclubs = req.body.clubs
        const userf = users.find(user => user.id === userid )
-      
+       userf.type=updatedtype
          userf.firstname = updatedfname
         userf.lastname = updatedlname
-         userf.birth_date = updatedbdate
-        userf.clubs = updatedclubs
         userf.email = updatedemail
         userf.password = updatedpass
-         userf.type=updatedtype
+         userf.birth_date = updatedbdate
+         userf.bio = updatedbio
          userf.house = updatedhouse
          userf.score = updatedscore
           userf.din = updatedin
          userf.dor = updatedor
-         userf.bio = updatedbio
-         
+         userf.clubs = updatedclubs
 
     res.send(users)
     })
-   
+    //delete a user
+    router.delete('/',(req,res)=>{
+        const userid =req.body.id;
+        
+        const userf = users.find(user => user.id === userid )
+        const index = users.indexOf(userf)
+        users.splice(index,1)
+        res.send(users)
+    }
+    )
+
+ 
 module.exports = router
