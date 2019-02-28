8fcd4fe23251f7dff2fa4f05faf824706daca18b
NadaBotros <47039130+NadaBotros@users.noreply.github.com>
2019-02-28 19:40:35 +0200

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,56 +1,23 @@
 const uuid = require('uuid')
 const express = require('express')
 const Joi = require('joi')
-const user = require('./models/user')
-const adminschema = require('./schemas/TIQadmin')
-const userschema = require('./schemas/hubuser') 
-const discipleschema = require ('./schemas/disciples')
-const parentschema = require('./schemas/parent')
+const user = require('../../models/user')
+const alumnischema = require('../../schemas/alumni')
+const memberschema = require('../../schemas/member') 
+//const discipleschema = require ('./schemas/disciples')
+//const parentschema = require('./schemas/parent')
 const router = express.Router();
 
 
 
 
 
-var User 
 
-const user = [
+let newuser
 
-
-    {
-        type:'member',
-        firstname:'Mahy',
-        lastname: 'Ali',
-        birth_date:'23-4-1994',
-        bio: 'expert debater ',
-        email: 'mali@gmail.com',
-        password:'mahy94',
-        house:'Pegasus',
-        score: 278,
-        din:'1-10-2017',
-        dor:null,
-        clubs:['TIQ']
+let users = [
 
 
-
-    },
-
-    {
-
-        type:'supervisor',
-        firstname:'Hania',
-        lastname: 'Essam',
-        birth_date:'26-6-1995',
-        bio: 'Logoistics supervisor ',
-        email: 'hania@gmail.com',
-        password:'hania123',
-        house:'neutral',
-        score: null,
-        din:'1-11-2018',
-        dor:null,
-        clubs:['TIQ']
-
-    },
 
     {
 
@@ -59,13 +26,14 @@ const user = [
     lastname: 'Marwan',
     birth_date:'23-6-2000',
     bio: 'young hunter ',
-    email: 'maro@yahoo.com',
+    email: 'maro@student.guc.edu.eg',
     password:'marrroo',
     house:'Orion',
     score: 198,
     din:'1-10-2019',
     dor:'1-11-2019',
-    clubs:['TIQ']
+    clubs:['TIQ'],
+    id:1
 
 },
 
@@ -76,13 +44,14 @@ const user = [
     lastname: 'Botros',
     birth_date:'23-9-1998',
     bio: ' Sun Hunter ',
-    email: 'nbotros@gmail.com',
+    email: 'nbotros@student.guc.edu.eg',
     password:'nbtros98',
     house:'Orion',
     score: 341,
     din:'1-11-2018',
     dor:'1-11-2019',
-    clubs:['TIQ','MUN']
+    clubs:['TIQ','MUN'],
+    id:2
 
 }
 
@@ -90,7 +59,7 @@ const user = [
 
 
 
-router.post('/joi',(req,res)=> 
+router.post('/add',(req,res)=> 
                     {
                         const firstname = req.body.firstname
                         const lastname = req.body.lastname
@@ -103,49 +72,40 @@ router.post('/joi',(req,res)=>
                         const score=req.body.score
                         const din = req.body.din
                         const dor = req.body.dor
+                        const bio=req.body.bio
                 
-                    });
+                    
 switch(req.body.type)
 {
-    case(alumni):
+    case('alumni'):
             const alumni =Joi.validate(req.body,alumnischema)
 
-            if(alumni.error) return res.status(404).send({error:alumni.error.details[0].message})
-            User.push(new user(type,firstname,lastname,birth_date,bio,email,password,house,score,din,dor,clubs))
-            return res.json({data:User});
+            if(alumni.error) return res.status(400).send({error:alumni.error.details[0].message})
+            newuser=new user(type,firstname,lastname,birth_date,bio,email,password,house,score,din,dor,clubs)
+            users.push(newuser)
+           // res.send(users)
+           return res.send({data:newuser});
+          
 
 
-
-
-
-
-case(member):
+case('member'):
             const member =Joi.validate(req.body,memberschema)
-            if(member.error) return res.status(404).send({error:member.error.details[0].message})
-            User.push(new user(type,firstname,lastname,birth_date,bio,email,password,house,score,din,dor,clubs))
-            return res.json({data:User});
+            if(member.error) return res.status(400).send({error:member.error.details[0].message})
+            newuser=new user(type,firstname,lastname,birth_date,bio,email,password,house,score,din,dor,clubs)
+            users.push(newuser)
+           // res.send(users)
+           return res.send({data:newuser});
+
 
    
 
 }
-  
+});  
 
 router.get('/',(req,res)=>{
-    res.send({data:User})
+    res.send({data:users})
 })
 
-router.delete('/:id',(req,res)=>{
-    const userid = req.params.id 
-
-    const u = User.find(user => user.id === userid)
-
-    const index = users.indexOf(u)
-
-    User.splice(index,1)
-
-    res.send(User)
-
-})
 
 
 
@@ -158,11 +118,10 @@ router.delete('/:id',(req,res)=>{
 // Update a user (alumni or members)
 
 
-// Update a book's title
 
-app.put('/api/users/:id', (req, res) => {
+router.put('/', (req, res) => {
 
-    const userid = req.params.id 
+    const userid = req.body.id 
 
     const updated_firstname = req.body.firstname
     const updated_lastname = req.body.lastname
