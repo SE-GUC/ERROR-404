4da9f6eddc28b24c2ff0d45f3a9d8deba7368678
NadaBotros <47039130+NadaBotros@users.noreply.github.com>
2019-02-28 20:16:59 +0200

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const users = require('./routes/api/Users')
+const app = express()
+app.use(express.json())
+app.use('/api/Users', users)
+
+
+app.use((req, res) => {
+    res.status(404).send({err: 'We can not find what you are looking for'});
+ })
+
+ 
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Server up and running on port ${port}`))
