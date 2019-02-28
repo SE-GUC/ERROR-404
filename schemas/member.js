fc1c4ace061c38abe2da751fc468fefaea7b178f
NadaBotros <47039130+NadaBotros@users.noreply.github.com>
2019-02-28 19:44:03 +0200

    Update member.js

--- schemas/member.js
+++ schemas/member.js
@@ -3,17 +3,17 @@ const TIQmemberschema =
 {
     firstname : Joi.string().min(3).required(),
     lastname : Joi.string().min(3).required(),
-    birth_date : Joi.date().format('DD-MM-YYYY').required(),
+    birth_date : Joi.string().regex(/[0-3][0-9]\-[0-1][0-2]\-[1-2][0-9][0-9][0-9]/).required(),
     clubs : Joi.array().required(),
     email : Joi.string().regex(/[a-z0-9\.\-]+\@student\.guc\.edu\.eg$/).required(),
     password : Joi.string().min(8).required(),
-    house :  Joi.string().options("Pegasus","Orion","Neutral"),
-    score : Joi.int().optional(),
-    bio : Joi.description(string),
-    type : Joi.options("TIQmember")   
+    house :  Joi.string(),
+    score : Joi.number(),
+    bio : Joi.string(),
+    type : Joi.string(),
+    din : Joi.string(),
+    dor : Joi.string()   
 
 };
 
 module.exports= TIQmemberschema;
-
-
