f89a4dc5a71f5900fba9e7dfadb6ea718e206c06
Bassant Elnaggar <47534992+bassantelnaggar@users.noreply.github.com>
2019-03-01 12:05:42 +0200

    Update and rename user.js to User.js

--- models/User.js
+++ models/User.js
@@ -1,11 +1,11 @@
 const uuid = require ('uuid')
 //User model
-class user {
-    constructor(type,firstname,lastname,birth_date,bio,email,password,house,score,din,dor,clubs){
+class User {
+    constructor(type,firstName,lastName,birthDate,bio,email,password,house,score,din,dor,clubs){
         this.type=type;
-        this.firstname=firstname;
-        this.lastname=lastname;
-        this.birth_date=birth_date;
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.birthDate=birthDate;
         this.bio=bio;
         this.email=email;
         this.password=password;
@@ -17,4 +17,4 @@ class user {
         this.id=uuid.v4();
     };
 };
-module.exports=user;
+module.exports=User;
