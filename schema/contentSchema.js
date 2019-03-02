8151928fd272c59dd504f272f123e780efbbc3eb
Kamal Salah <47534555+kamalsalah32@users.noreply.github.com>
2019-03-02 21:31:36 +0200

    Update contentSchema.js

--- schema/contentSchema.js
+++ schema/contentSchema.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 const contentSchema = 
 {
-    date : Joi.date().format('DD-MM-YYYY').required(),
+    date : Joi.string().regex(/[0-3][0-9]\-[0-1][0-9]\-[1-2][0-9][0-9][0-9]/).required(),
     type : Joi.options("debate", "event", "reqruitment"),
     description : Joi.description(string)    
 
