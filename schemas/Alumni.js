1c6a3b2ddccc5ec68d409052363f89c477f52362
Bassant Elnaggar <47534992+bassantelnaggar@users.noreply.github.com>
2019-03-02 19:44:54 +0200

    Update Alumni.js

--- schemas/Alumni.js
+++ schemas/Alumni.js
@@ -3,7 +3,7 @@ const TIQalumnischema =
 {
     firstname : Joi.string().min(3).required(),
     lastname : Joi.string().min(3).required(),
-    birth_date : Joi.string().regex(/[0-3][0-9]\-[0-1][0-2]\-[1-2][0-9][0-9][0-9]/).required(),
+    birth_date : Joi.string().regex(/[0-3][0-9]\-[0-1][0-9]\-[1-2][0-9][0-9][0-9]/).required(),
     clubs : Joi.array().required(),
     email : Joi.string().regex(/[a-z0-9\.\-]+\@student\.guc\.edu\.eg$/).required(),
     password : Joi.string().min(8).required(),
