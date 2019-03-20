f6e24250fc8eb22c08b058178efab7294034bc65
NadaBotros <47039130+NadaBotros@users.noreply.github.com>
2019-03-20 19:22:43 +0200

    Add files via upload

---
+++ models/Article.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+
+const articleSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
+    comments: {
+        type: String, 
+        required: true
+    }
+   
+})
+
+module.exports = User = mongoose.model('users', articleSchema)
