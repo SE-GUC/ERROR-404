bae8e6b5114abcac4f4b68a21ac0114029c319ca
Bassant Elnaggar <47534992+bassantelnaggar@users.noreply.github.com>
2019-03-20 20:14:10 +0200

    Add files via upload

---
+++ validations/articleValidations.js
@@ -0,0 +1,28 @@
+const Joi = require('joi')
+
+
+module.exports = {
+    createValidation: request => {
+        const createSchema = {
+            title: Joi.string().required(),
+            description: Joi.string().required(),
+            author: Joi.string().required(),
+            date: Joi.string().required(),
+            comments: Joi.string().required()
+        }
+
+        return Joi.validate(request, createSchema)
+    },
+
+    updateValidation: request => {
+        const updateSchema = {
+            title: Joi.string(),
+            description: Joi.string(),
+            author: Joi.string(),
+            date: Joi.string()
+            
+        }
+
+        return Joi.validate(request, updateSchema)
+    }, 
+}
