b4c6af81ff7600e6d63141dbdbd0061eb3f6daa5
Bassant Elnaggar <47534992+bassantelnaggar@users.noreply.github.com>
2019-03-20 20:14:55 +0200

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+if(process.env.NODE_ENV === 'production')
+modules.exports=  require('./keys_prod')
+else
+module.exports = require('./keys_dev')
---
+++ config/keys_dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://bassant98:<bassant98>@trail-mflro.mongodb.net/test?retryWrites=true',
+}
