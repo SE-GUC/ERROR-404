cac00f3f21cba0a725b41491622e9095c23812ea
Bassant Elnaggar <47534992+bassantelnaggar@users.noreply.github.com>
2019-03-22 21:07:17 +0200

    Update userValidations.js

--- validations/userValidations.js
+++ validations/userValidations.js
@@ -3,25 +3,19 @@ module.exports={
     updateUserValidation: request =>{
         const updateUserSchema={
             
-    firstname : Joi.string().min(3),
-    lastname : Joi.string().min(3),
-    birth_date : Joi.string().regex(/[0-3][0-9]\-[0-1][0-9]\-[1-2][0-9][0-9][0-9]/),
-    clubs: Joi.array(),
-    password : Joi.string().min(8),
-    bio : Joi.string(),
-     
-    clubs : Joi.array(),
-    house :  Joi.string(),
-    score : Joi.number(),
-    type : Joi.string(),
-    din : Joi.string(),
-    dor : Joi.string()   
+            type : Joi.string(),
+            firstName : Joi.string().min(3),
+            lastName : Joi.string().min(3),
+            birthDate : Joi.string().regex(/[0-3][0-9]\-[0-1][0-2]\-[1-2][0-9][0-9][0-9]/),
+            bio : Joi.string(),
+            email : Joi.string().regex(/[a-z0-9\.\-]+\@student\.guc\.edu\.eg$/),
+            password : Joi.string().min(8),
+            house :  Joi.string(),
+            score : Joi.number(),
+            dor:Joi.string(),
+            din:Joi.string(),
+            clubs : Joi.array()
        }
        return Joi.validate(request,updateUserSchema)
     }
 }
-
-// Question: hwa ezay afra2 maben TIQ admin and user fe el update
-// Question : eh el far2 maben en ana 23ml required fe model w required fe model
-// mongo ?
-//
