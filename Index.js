929eaeade1d0baed86ecdbe823fe590af1395ba6
NadaBotros <47039130+NadaBotros@users.noreply.github.com>
2019-03-23 17:44:58 +0200

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,24 +1,55 @@
-
 const express = require('express')
-
-const users = require('./routes/api/Users')
+const mongoose = require('mongoose')
 
 const app = express()
-
+const cors = require('cors')
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send(`<a href="/api/Users">Users</a>`);
-})
 
-// Direct routes to appropriate files 
+
+
+// Require Router Handlers
+const users = require('./routes/api/users')
+const articles = require('./routes/api/Articles')
+const debates = require('./routes/api/Debates')
+
+
+//DB config 
+const db=require('./config/keys').mongoURI
+
+
+// Connect to mongo
+mongoose
+    .connect(db,{useNewUrlParser: true })
+    .then(() => console.log('Connected to MongoDB'))
+.catch(err => console.log(err))
+
+// Init middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cors())
+
+
+//Entry point
+app.get('/', (req,res) => res.send(`<h1>Heloooooosss</h1>`))
+app.get('/Users', (req,res) => res.send(`<h1>hey user</h1>`))
+app.get('/Articles', (req,res) => res.send(`<h1>Articlesssssssssssssss?</h1>`))
+app.get('/Users/register', (req,res) => res.send(`<h1>ready to registe?</h1>`))
+
+
+
+
 app.use('/api/Users', users)
+app.use('/api/Articles',articles)
+app.use('/api/Debates', debates)
 
-// Handling 404
- app.use((req, res) => {
-     res.status(404).send({err: 'We can not find what you are looking for'});
-  })
 
-const port = 3000
+
+app.use((req, res) => {
+    res.status(404).send({err: 'We can not find what you are looking for'});
+ })
+
+ 
+
+const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Server up and running on port ${port}`))
-
