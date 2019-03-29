496d2b00b819f9b64bcede24745010a158b85d31
Bassant Elnaggar <47534992+bassantelnaggar@users.noreply.github.com>
2019-03-29 19:09:01 +0200

    Chatbar Validation

---
+++ validations/chatBarValidations.js
@@ -0,0 +1,17 @@
+const Joi = require('joi')
+module.exports = {
+    
+    updateValidation: request => {
+        const updateSchema = {
+            debateLiveTitle: Joi.string(),
+            date: Joi.string(),
+            numberOfResponses: Joi.number(),
+            forResponses: Joi.array(),
+            againstResponses:Joi.array()
+        }
+
+        return Joi.validate(request, updateSchema)
+
+    }
+     
+}
