2809b00d2bddeaf283723e8fae9310e8a46f38b2
NadaBotros <47039130+NadaBotros@users.noreply.github.com>
2019-03-31 19:04:06 +0200

    Add files via upload

--- routes/api/Chatbars.js
+++ routes/api/Chatbars.js
@@ -4,7 +4,48 @@ const mongoose = require('mongoose')
 const Chatbars = require('../../models/Chatbar')
 const chatBarValidator = require('../../validations/chatBarValidations')
 
-router.put('/for/:id',async(req,res)=>{
+
+
+
+
+
+
+
+router.get('/',async (req,res)=>{
+    const chats = await Chatbars.find()
+    res.json({data:chats})
+})
+
+
+
+                              
+router.post('/create', async (req,res) => {
+    try {
+     const isValidated = chatBarValidator.createValidation(req.body)
+    
+     if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
+ 
+    //  const newChatBar = await chatbars.create(req.body)
+     const { debateLiveTitle,date} = req.body
+     const newMotion = new Chatbars({
+        debateLiveTitle ,
+        date ,
+        numberOfResponses:0
+      
+     
+  })
+  
+  const newChatBar=await Chatbars.create(newMotion)
+          
+return res.json({msg:'A new chatBar was created successfully :)', data: newChatBar})
+    }
+    catch(error) {
+        // We will be handling the error later
+        console.log(error)
+    }  
+ }),
+
+ router.put('/for/:id',async(req,res)=>{
  
     try{
         const chatBarId=req.params.id
@@ -45,4 +86,25 @@ router.put('/against/:id',async(req,res)=>{
         console.log(error)
     }
 })
-module.exports = router
+router.delete('/:id', async (req,res) => {
+    try {
+     const id = req.params.id
+     const deleteChatBar = await Chatbars.findByIdAndRemove({_id:id})
+     res.json({msg:'That Chat-Bar was deleted successfully', data: deleteChatBar})
+    }
+    catch(error) {
+        console.log(error)
+    }  
+ })
+ 
+
+router.get('/:id',async (req,res)=>{
+    
+    const motionId = req.params.id
+  
+    const motion = await Chatbars.findById({_id:motionId})
+    res.send(motion)
+   
+    })
+
+module.exports = router;
