b0d9b4c22ce1280fe17bc1e2769e8b2ef1b0c28f
yousseflotfy <47537089+yousseflotfy@users.noreply.github.com>
2019-03-31 20:02:40 +0200

    Add files via upload

--- fn.js
+++ fn.js
@@ -1,99 +1,26 @@
-//const jest = require('jest')
-const axios = require('axios')
-const User = require('./models/User')
-const Users = require('./routes/api/Users')
-const Content = require('./models/Content')
-const Contents = require('./routes/api/Contents')
-const functions =
-{
-    getUsers: async()=>{
-        const Users = await axios.get('http://localhost:3000/api/Users')
-        return Users
-    },
-    getUserByIdFound : async()=>{
-        const user =  User.findOne()
-        const userId = user._id
-        const userFound = await axios.get('http://localhost:3000/api/Users/'+userId)
-        
+const axios = require('axios');
 
-        return userFound
+functions = {
 
+    
+    getAllClubs : async () => {
+        const clubs = await axios.get('http://localhost:3000/api/Clubs');
+        return clubs;
     },
-    getUserByIdNotFound : async()=>{
-        const user = await axios.get('http://localhost:3000/api/Users/1')
-        return user
-
+    createContent : async content => {
+        const newContent = await axios.post('http://localhost:3000/api/Contents', content);
+        return newContent;
     },
-    createUserTesting : async() =>{
-        const user = await axios.post('http://localhost:3000/api/Users/register',{
-            
-            type:"member",
-            firstName:"Nadin ",
-            lastName:"7ob 3omry",
-            birthDate:"12-11-1998",
-            bio:"heheehhe",
-            email:"nadin@student.guc.edu.eg",
-            password:"boooom1234",
-            clubs:["tiq"],
-            house:"pegasus"
-            
-        })
-        return user
+    updateContent : async (id, updatedData) => {
+        const updatedContent = await axios.put(`http://localhost:3000/api/Contents/${id}`, updatedData);
+        return updatedContent;
     },
-
-    updateUserScore : async() =>{
-        const user = await axios.post('http://localhost:3000/api/Users/register',{
-            
-            type:"member",
-            firstName:"Nadin ",
-            lastName:"7ob 3omry",
-            birthDate:"12-11-1998",
-            bio:"heheehhe",
-            email:"nadin@student.guc.edu.eg",
-            password:"boooom1234",
-            clubs:["tiq"],
-            house:"pegasus"
-            
-        })
-        const id = user.data.data._id
-        const updated = await axios.put('http://localhost:3000/api/Users/'+id+'/7')
-        return updated
-    },
-    deleteUserSuccess : async()=>{
-        //const user =  await createUserTesting()
-        const user = await axios.post('http://localhost:3000/api/Users/register',{
-            
-            type:"member",
-            firstName:"Nadin ",
-            lastName:"7ob 3omry",
-            birthDate:"12-11-1998",
-            bio:"heheehhe",
-            email:"nadin@student.guc.edu.eg",
-            password:"boooom1234",
-            clubs:["tiq"],
-            house:"pegasus"
-            
-        })
-        const id = user.data.data._id
-        //console.log(id)
-        const deleteduser = await axios.delete('http://localhost:3000/api/Users/'+id)
-        return deleteduser
-    },
-    deleteUserFailed : async()=>{
-        const deleteuser = await axios.delete('http://localhost:3000/api/Users/2')
-        
-        return deleteuser
-    },
-    viewcontent : async()=>{
-        const getuser = await axios.get('http://localhost:3000/api/Contents/')
-        return getuser
-    },
-    viewcertaincontent : async()=>{
-        const allContent = await axios.get('http://localhost:3000/api/Contents')
-        const contId = allContent.data.data[0]._id
-        const getcertainuser = await axios.get('http://localhost:3000/api/Contents/'+contId)
-        return getcertainuser
+    deleteContent : async id => {
+        const deletedContent = await axios.delete(`http://localhost:3000/api/Contents/${id}`);
+        return deletedContent;
     }
-}
-module.exports = functions
 
+
+}
+
+module.exports = functions;
