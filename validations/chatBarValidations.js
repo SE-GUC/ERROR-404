b053fc4d5002d2b6ab126911d0a99578894df091
Kamal Salah <47534555+kamalsalah32@users.noreply.github.com>
2019-03-31 11:52:20 +0200

    Add files via upload

--- validations/chatBarValidations.js
+++ validations/chatBarValidations.js
@@ -1,3 +1,4 @@
+    
 const Joi = require('joi')
 module.exports = {
     
@@ -12,6 +13,17 @@ module.exports = {
 
         return Joi.validate(request, updateSchema)
 
+    },
+    createValidation: request => {
+        const createSchema = {
+            debateLiveTitle: Joi.string().required(),
+            date: Joi.string(),
+            numberOfResponses: Joi.number(),
+            forResponses: Joi.array(),
+            againstResponses:Joi.array()
+        }
+
+        return Joi.validate(request, createSchema)
     }
      
 }
