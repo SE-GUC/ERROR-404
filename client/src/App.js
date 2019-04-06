8b2b4d3441fba3c8d3b26a3d65fe56d87448eac1
kamalsalah32 <kamalsalah32@gmail.com>
2019-04-07 00:30:29 +0200

    React CRUD for clubs

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import AllClubs from './components/AllClubs';
+import AllClubs from './components/Clubs/AllClubs';
 import axios from 'axios';
-import AddClub from './components/AddClub';
+import AddClub from './components/Clubs/AddClub';
+import DeleteClub from './components/Clubs/DeleteClub';
+import UpdatingClubs from './components/Clubs/UpdatingClubs';
 import './App.css';
 
 class App extends Component {
   state = {
-    allClubs: ['club1', 'club2', 'club3']
+    allClubs: []
   }
 
   componentDidMount() {
@@ -18,6 +20,35 @@ class App extends Component {
     axios.post('/api/Clubs' , club)
       .then(res => this.setState({allClubs: [...this.state.allClubs, res.data.data]})); 
   }
+
+  delClub = id => {
+    axios.delete(`/api/Clubs/${id}`)
+      .then(this.setState({allClubs: [...this.state.allClubs.filter(club => club._id !== id)]}))
+    
+  }
+
+  updateClub = club => {
+    console.log(club)
+    const clubs = this.state.allClubs;
+    var i;
+    for(i = 0; i < clubs.length; i++){
+      if(clubs[i]._id === club._id){
+        if(club.name !== "")
+          clubs[i].name = club.name
+        if(club.description !== "")
+        clubs[i].description = club.description
+      }
+      
+    }
+    const updatedData = {}
+    if(club.name !== "")
+      updatedData.name = club.name
+      if(club.description !== "")
+      updatedData.description = club.description
+    axios.put(`/api/Clubs/${club._id}`, updatedData)
+      .then(this.setState({ allClubs: clubs }))
+    
+  }
   
   render() {
     return (
@@ -26,6 +57,10 @@ class App extends Component {
         <AllClubs allClubs = {this.state.allClubs}/>
         <h1>ADD NEW CLUB</h1>
         <AddClub addClub = {this.addClub}/>
+        <h1>DELETE CLUB</h1>
+        <DeleteClub allClubs = {this.state.allClubs} delClub = {this.delClub}/>
+        <h1>UPDATE CLUB</h1>
+        <UpdatingClubs allClubs = {this.state.allClubs} updateClub = {this.updateClub}/>
       </div>
     );
   }
