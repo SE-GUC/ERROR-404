ca302e4a85c5e3250bd8f9c43432c645dea84269
yousseflotfy <youssef.medhat@student.guc.edu.eg>
2019-04-07 16:31:24 +0200

    app commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,15 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './App.css';
-import AllArticles from './components/AllArticles'
-import AddArticle from './components/AddArticle'
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
-  state = {
-    allArticles : [] 
-  }
-  componentDidMount(){
-    axios.get('/api/Articles')
-    .then(res=>this.setState({allArticles:res.data.data}))
-  }
-
-  addArticle = (article)=>{
-      console.log(article)
-      axios.post('http://localhost:5000/api/Articles/create' , article)
-      .then(res=>this.setState({allArticles:[...this.state.allArticles,res.data.data]}))
-      .catch(error=> {
-        console.log('erorr')
-      });
-  }
-  render() {
+  render(){
     return (
-      <div className="App">
-      <h1>show all Articles</h1>
-      <AllArticles allArticles = {this.state.allArticles}/>
-      <h1>add new article</h1>
-      <AddArticle addArticle = {this.addArticle} />
-      </div>
+    <div className = "App">
+    <h1>TIQ</h1>
+    </div>
     );
   }
 }
+
 
 export default App;
