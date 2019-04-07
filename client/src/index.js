e2137908bac3f04bbacad66bc9c817e125072d11
nouranayad98 <nouran.ayad@student.guc.edu.eg>
2019-04-07 15:38:31 +0200

    final

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import FAQ from './components/faq/FAQ';
+
+import * as serviceWorker from './serviceWorker';
+import { Route, BrowserRouter as Router  , Switch} from 'react-router-dom'
+// import 'typeface-roboto'
+
+const routing = (
+    <Router>
+      <div>
+        <hr />
+        <Switch>
+        <Route exact path="/" component={App} />
+        <Route exact path="/faq" component={FAQ} />
+
+        </Switch>
+      </div>
+    </Router>
+  )
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
