150c5fe227df2b5de6d34aacc7e252867ecfb456
nouranayad98 <nouran.ayad@student.guc.edu.eg>
2019-04-07 22:59:16 +0200

    seif

--- client/src/components/faq/FAQ.js
+++ client/src/components/faq/FAQ.js
@@ -14,6 +14,33 @@ class FAQ extends Component {
     axios.get('http://localhost:5000/api/FAQs')
     .then(res => this.setState({ FAQs: res.data.data }))
   }
+  delfaq = (id) => {
+    axios.delete('http://localhost:5000/api/FAQs/'+id)
+      .then(res => this.setState({ FAQs: [...this.state.FAQs.filter(faq => faq._id !== id)] }));
+ 
+}
+updatefaq = (id,question,answer) => {
+  console.log(id);
+  console.log(question);
+  console.log(answer);
+  axios.put('http://localhost:5000/api/FAQs/edit/'+id,
+  {
+    "answer":answer,
+    "question":question
+  })
+  .then(res => {
+    axios.get('http://localhost:5000/api/FAQs')
+    .then(res => this.setState({ FAQs: res.data.data }))
+    // const temp={
+    //   _id:id,
+    //   question:question,
+    //   answer:answer
+    // }
+    // this.setState({ FAQs: [...this.state.FAQs.filter(faq => faq._id !== id)] })
+    // this.setState({ FAQs: [...this.state.FAQs,temp] })
+  });
+
+}
   addFAQ = (question,answer) => {
     axios.post('http://localhost:5000/api/FAQs/add', {
       question,
@@ -28,7 +55,7 @@ class FAQ extends Component {
           <div className="container">
             <h1>FAQs</h1>      
             <AddFaq addFAQ={this.addFAQ} />       
-            <FAQs  FAQs={this.state.FAQs}  />
+            <FAQs  FAQs={this.state.FAQs} delfaq={this.delfaq} updatefaq={this.updatefaq} />
              
           </div>  
         </div>
